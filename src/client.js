c4b812618e7f179745c2497b4f2c1cd10de16d84
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-01-13 15:02:35 +0100

    Build broadcaster stream that will broadcast a message from the client to all other connected users

--- src/client.js
+++ src/client.js
@@ -4,7 +4,15 @@ import { Writable } from 'node:stream';
 
 const output = Writable({
   write(chunk, encoding, callback) {
-    stdout.write(chunk);
+    const { id, message } = JSON.parse(chunk);
+
+    if (message) {
+      stdout.write(message);
+    } else {
+      stdout.write('...');
+    }
+    callback();
   },
 });
+
 process.stdin.pipe(net.connect(3000)).pipe(output);
--- src/server.js
+++ src/server.js
@@ -6,9 +6,27 @@ import dotenv from 'dotenv/config';
 
 const users = new Map();
 
-const port = process.PORT || 9000;
+const notifyUsers = (socketId, data) => {
+  [...users.values()]
+    .filter((userSocket) => userSocket.id !== socketId)
+    .forEach((userSocket) => userSocket.Write(data));
+};
+
+const streamProadcaster = (socket) => {
+  return Writable({
+    write(chunk, encoding, callback) {
+      const data = JSON.stringify({
+        message: chunk.toString(),
+        id: socket.id,
+      });
+      notifyUsers(socket.id, data);
+      callback();
+    },
+  });
+};
+
 const server = net.createServer((socket) => {
-  socket.write(socket);
+  socket.write(streamProadcaster(socket));
 });
 
 server.listen(port, () =>
@@ -18,9 +36,11 @@ server.listen(port, () =>
   )
 );
 
+const port = process.PORT || 9000;
 server.on('connection', (socket) => {
   socket.id = randomUUID();
   console.log(`New Connection: ${socket.id}`.bgBlue.bold.white);
+  users.set(socket.id, socket);
   socket.write(
     JSON.stringify({
       id: socket.id.slice(0, 4),
@@ -29,6 +49,6 @@ server.on('connection', (socket) => {
 });
 
 server.on('close', (socket) => {
-  users.delete(socket.id);
   console.log(`User [${socket.id}] is deconnected`.bgRed.bold.white);
+  users.delete(socket.id);
 });
